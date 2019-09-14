a1e48a2b6765f0fffc113fc383f3aeed2247238d
Shamel Lakin <christlife4u@gmail.com>
2019-09-14 19:51:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,4 @@
+require 'pry'
 number = 10
 
 def countdown
@@ -6,14 +7,14 @@ def countdown
   puts "#{number} SECOND(S)!"
 end
 puts "HAPPY NEW YEAR!"
+binding.pry
 
+# number = 10
 
-number = 10
-
-def countdown_with_sleep
-  while number <= 10
-  number -= 1
-  sleep(0.5)
-  puts "#{number} SECOND(S)!"
-end
-puts "HAPPY NEW YEAR!"
+# def countdown_with_sleep
+#   while number <= 10
+#   number -= 1
+#   sleep(0.5)
+#   puts "#{number} SECOND(S)!"
+# end
+# puts "HAPPY NEW YEAR!"
